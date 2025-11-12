b648732c83834ff6fde2c1a5983320f25c8d3290
TrofimovVladislav5 <vladmyc095@gmail.com>
2025-11-12 07:39:57 +0300

    wip: cyclic buffer, pwm

---
+++ lab2/3/bench/test.v
@@ -0,0 +1,37 @@
+`timescale 1ns/1ps
+
+module test;
+    reg clk;
+    reg reset;
+    reg btn;
+    wire [6:0] segments;
+
+    cyclic_buffer uut (
+        .clk(clk),
+        .reset(reset),
+        .btn(btn),
+        .segments(segments)
+    );
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        reset = 1;
+        btn = 0;
+        #20;    
+        reset = 0;
+
+        #10 btn = 1; #10 btn = 0;
+        #20 btn = 1; #10 btn = 0;
+        #20 btn = 1; #10 btn = 0;
+
+        #100;
+    end
+
+    initial begin
+        $display("Time | Buffer | Segments");
+        $monitor("%0t | %d | %b", $time, uut.buffer, segments);
+    end
+
+endmodule
---
+++ lab2/3/src/cyclic_buffer.v
@@ -0,0 +1,28 @@
+module cyclic_buffer(
+    input wire clk,
+    input wire reset,
+    input wire btn,
+    output wire [6:0] segments
+);
+
+    reg[3:0] buffer;
+    reg btn_previous;
+
+    wire btn_pressed = btn & (~btn_previous);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            btn_previous <= 0;
+            buffer <= 0;
+        end else begin
+            btn_previous <= btn;
+            if (btn_pressed)
+                buffer <= buffer + 1;
+        end
+    end
+    ssegment_display show(
+        .value(buffer),
+        .segments(segments)
+    );
+endmodule
+
